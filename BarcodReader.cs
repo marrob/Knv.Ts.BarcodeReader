3a0bd33ff51ae1c09eda9aeeb722c39b23e0fbc9
marrob <marrob50hz@gmail.com>
2023-08-11 09:38:56 +0200

    first

---
+++ BarcodReader.cs
@@ -0,0 +1,72 @@
+﻿namespace Knv.Ts.BarcodeReader
+{
+    using System;
+    using System.IO;
+    using System.Net.Sockets;
+    using System.Text;
+
+    public static class BarcodReader
+    {
+ 
+        const int COMMAND_PORT = 9011;
+        const int DATA_PORT = 9012;
+
+        public static string Read(string resource, string type, int timeoutMs, bool simulation)
+        { 
+            return Read(resource, type, timeoutMs, COMMAND_PORT, DATA_PORT, simulation);
+        }
+
+        public static string Read(string resource, string type, int timeoutMs, int cmdPort, int dataPort, bool simulation)
+        {
+            string barcode = "";
+            if (simulation)
+            {
+                barcode = "1234567890";
+            }
+            else
+            {
+                using (TcpClient cmdClient = new TcpClient(resource, COMMAND_PORT))
+                {           
+                    using (TcpClient dataClient = new TcpClient(resource, DATA_PORT))
+                     {
+                        using (NetworkStream cmdStream = cmdClient.GetStream())
+                        {
+                            dataClient.ReceiveTimeout = timeoutMs;
+                            using (NetworkStream dataStream = dataClient.GetStream())
+                            {
+                                Exception lastException = null;
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    try
+                                    {
+                                        string triggerCmd = "LON\r\n";
+                                        cmdStream.Write(Encoding.ASCII.GetBytes(triggerCmd), 0, triggerCmd.Length);
+
+                                        byte[] data = new byte[512];
+                                        int receivedDataLength = dataStream.Read(data, 0, data.Length);
+                                        barcode = Encoding.ASCII.GetString(data, 0, receivedDataLength).Trim();
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        lastException = ex;
+                                    }
+                                    finally
+                                    { 
+                                        string offCmd = "LOFF\r\n";
+                                        cmdStream.Write(Encoding.ASCII.GetBytes(offCmd), 0, offCmd.Length);
+                                    }
+
+                                    if (barcode != "")
+                                        break;
+                                }
+                                if (lastException != null)
+                                    throw new Exception($"The barcode reader cannot read... Details:{lastException.Message}");
+                            }
+                        }
+                    } 
+                }
+            }
+            return barcode;
+        }
+    }
+}
---
+++ Reader_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Knv.Ts.BarcodeReader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class Reader_UnitTest
+    {
+
+        [Test]
+        public void FakeReader()
+        {
+            var resp = BarcodReader.Read("", "", 1, true);
+            Assert.AreEqual("1234567890", resp);
+        }
+
+
+        [Test]
+        public void ECUTSReader()
+        {
+            var resp = BarcodReader.Read(resource:"169.254.160.42", type: "LAN", timeoutMs: 2000, simulation: false);
+            Assert.AreEqual("TT21460200002", resp);
+        }
+    }
+}
